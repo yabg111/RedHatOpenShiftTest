3c654a1f37b8942c4fa1744f0c96316ceed45dd8
yabg111 <yabg111@gmail.com>
2026-07-22 07:41:13 +0000

    helllo-word app

---
+++ Application/src/app.js
@@ -0,0 +1,16 @@
+{
+  "name": "hello-word",
+  "version": "1.0.0",
+  "description": "application for workshop",
+  "repository": {
+    "type": "git",
+    "url": "app.js"
+  },
+  "license": "ISC",
+  "author": "",
+  "type": "commonjs",
+  "main": "app.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  }
+}
